871fe8d1a11438715e87237a5585a1c31b373eae
nhartner <nhartner@gmail.com>
2020-01-31 15:34:33 -0700

    initial version

---
+++ src/main/java/org/interledger/example/connector/extensions/GcpEventOverrideConfig.java
@@ -0,0 +1,37 @@
+package org.interledger.example.connector.extensions;
+
+import org.interledger.connector.events.PacketFullfillmentEvent;
+import org.interledger.connector.events.PacketRejectionEvent;
+import org.interledger.connector.gcp.GcpPacketResponseEventPublisher;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * Example of a Component that gets picked up and overrides a default bean implementation
+ */
+@Configuration
+@ConditionalOnProperty(value = "spring.cloud.gcp.pubsub.enabled", havingValue = "true")
+public class GcpEventOverrideConfig {
+
+  @Bean
+  @Primary // must be primary to override the default GcpPacketResponseEventPublisher
+  GcpPacketResponseEventPublisher myGcpPacketResponseEventPublisher(
+    @Value("${example.message:hello world}") String message) {
+    return new GcpPacketResponseEventPublisher() {
+      @Override
+      public void publish(PacketFullfillmentEvent event) {
+        System.out.println("Fulfillment says " + message);
+      }
+
+      @Override
+      public void publish(PacketRejectionEvent event) {
+        System.out.println("Rejection says " + message);
+      }
+    };
+  }
+
+}
